a69fdd9d7f841ac165ca6fb85f7d0188383734df
JA <nekimail@nekimail.com>
2020-06-07 00:43:33 -0400

    filter

--- Assets/JS/todo.js
+++ Assets/JS/todo.js
@@ -4,7 +4,37 @@ document.addEventListener('DOMContentLoaded',
                             {
                               var forma=document.querySelector("#forma");
                               var unos=document.querySelector("#unos");
+                              var filterList = document.querySelector("#filter-list");
                               var broj_taskova = 0;
+                              
+                              filterList.addEventListener('keydown',
+                                                           function(e)
+                                                              {
+                                                                 if (e.keyIdentifier == 'Enter' || e.keyCode == 13) 
+                                                                  {
+                                                                    if (e.target.nodeName == 'INPUT' && e.target.type == 'text') 
+                                                                      {
+                                                                         e.preventDefault();
+                                                                      }
+                                                                  }
+                                                              }           
+                                                          );
+                              filterList.addEventListener('keyup',filterisi);
+                              function filterisi(e)
+                                {
+                                  const tekst = e.target.value.toLowerCase();
+                                  for (let elem of document.querySelectorAll('.task-list-item'))
+                                    { 
+                                      if(elem.textContent.toLowerCase().indexOf(tekst) === -1) 
+                                        {
+                                          elem.style.display='none'; 
+                                        }
+                                      else
+                                        {
+                                          elem.style.display="grid";
+                                        }
+                                    }
+                                }
 
                               forma.addEventListener('submit',dodajTask);
 
@@ -20,6 +50,7 @@ document.addEventListener('DOMContentLoaded',
                                       var span = document.createElement('span');
                                       var img = document.createElement('img');
                                       var bojac = document.createElement('form');
+                                      li.className = "task-list-item";
                                       // ubaci ikonu za x
                                       img.src="Assets/Images/x-circle.svg";
                                       img.height=16;
@@ -49,7 +80,12 @@ document.addEventListener('DOMContentLoaded',
                                           document.querySelector('.brojac').textContent = "Broj preostalih taskova:" + broj_taskova;
                                           
                                         }
+                                      // ocisti input polje za novi unos 
+                                      unos.value = '';
+
                                     }
+                                  
+
                                   e.preventDefault();
                                 }
                             });     
